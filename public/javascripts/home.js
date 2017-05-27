925eb6b7537dfdd5a0f060214b6c4addcdc3a29b
Nuwan.Munasinghe <Nuwan.Munasinghe@trycake.com>
2017-05-27 15:43:10 +0530

    Change radius

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -55,7 +55,7 @@ function createPoints(map, data) {
             fillOpacity: 1,
             map: map,
             center: createCenter(data[city].center),
-            radius: 20,
+            radius: 10,
             clickable: true,
             title: 'hi'
         });
